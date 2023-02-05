0087dbba576b21bd86a5032b73e1fa6f1b4a9062
engageintellect <jc9361@gmail.com>
2023-02-04 16:14:55 -0800

    init3

---
+++ apps/init/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "ui";
+
+export default function Web() {
+  return (
+    <div>
+      <h1>Init</h1>
+      <Button label="click me" url="https://google.com" />
+    </div>
+  );
+}
